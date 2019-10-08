286e405a4f7f43a7f105964e149a9a2a0e5a7dcd
João Pedro Abreu de Souza <jp_abreu@id.uff.br>
2019-10-08 08:20:32 -0300

    Catch rest

--- mainMPI.c
+++ mainMPI.c
@@ -29,8 +29,8 @@ int main(int argc, char* argv[])
 {
 	int my_rank;
 	int p; // número de processos
-	float a=0.0, b=1.0; // intervalo a calcular
-	int n=1024; // número de trapezóides
+	float a=0, b=1.0; // intervalo a calcular
+	int n=2048; // número de trapezóides
 	float h; // base do trapezóide
 	float local_a, local_b; // intervalo local
 	int local_n; // número de trapezóides local
@@ -48,7 +48,14 @@ int main(int argc, char* argv[])
 	h = (b-a) / n;
 	local_n = n / p;
 	local_a = a + my_rank * local_n * h;
-	local_b = local_a + local_n * h;
+	if(my_rank != (p-1))
+	{
+		local_b = local_a + local_n * h;
+	}
+	else
+	{
+		local_b=b;
+	}
 	integral = calcula(local_a, local_b, local_n, h);
 	MPI_Reduce(&integral, &total,1, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
 	if(my_rank == 0) printf("Resultado: %f\n", total);
